23a567ef24d72e2070e73d7b82de685b709ff995
Ludmila Zhylianova <ludiky30@gmail.com>
2020-06-19 16:50:00 +0700

    first lesson

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+alert("Hello! My name is Mila! I can show message!");
+console.log("Also I can use the consol. It's very simple lesson");
