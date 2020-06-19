cd471aa9c845d5f265797a97a76ab228de3d2913
Ludmila Zhylianova <ludiky30@gmail.com>
2020-06-19 18:29:20 +0700

    Lessot 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Lesson 01
 let money;
 let income;
 let addExpenses;
@@ -6,3 +7,19 @@ let mission;
 let period;
 alert("Hello! My name is Mila! I can show message!");
 console.log("Also I can use the consol. It's very simple lesson");
+
+//Lesson02
+money = 50000;
+income = 10000;
+addExpenses = "Интернет, Такси, Коммуналка, Курсы в GloAcademy";
+deposit = true;
+mission = 100000;
+period = 5;
+console.log("Переменная money имеет тип " + typeof(money));
+console.log("Переменная money имеет тип " + typeof(income));
+console.log("Переменная money имеет тип " + typeof(deposit));
+console.log("Длина строки addExpenses равна " + addExpenses.length);
+console.log(`Период равен ${period} месяцев.\nЦель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split());
+let  budgetDay = (money + income) / 30;
+console.log(`Дневной бюджет равен ${budgetDay} рублей`);
