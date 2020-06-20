076bf1e4c45199a18a9b9e8838ff68edccf680ab
Ludmila Zhylianova <ludiky30@gmail.com>
2020-06-20 12:26:18 +0700

    lesson 4 hard

---
+++ js/script4-hard.js
@@ -0,0 +1,17 @@
+let myFun = function(val) {
+  if (typeof(val) === "string") {
+    alert("Это строка");
+    val = val.trim();
+    val = val.length > 30 ? val.slice(0,30)+"...": val;
+  }
+  return(val);
+}
+
+alert("Тест со строкой короче 30 символов");
+alert(myFun("Бесконечность - не предел!"));
+
+alert("Тест со строкой длинее 30 символов");
+alert(myFun("Бесконечность - не предел!!!!!!!!!!!!!!!!"));
+
+alert("Тест с числом");
+alert(myFun(5));
