8ac05cc0c2eec68a114078c5dde316ec3dbb60ab
Ludmila Zhylianova <ludiky30@gmail.com>
2020-06-22 17:40:32 +0700

    lesson 9

---
+++ js/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const startBtn = document.getElementById("start");
+
+const plus1 = document.getElementsByTagName("button")[0];
+const plus2 = document.getElementsByTagName("button")[1];
+
+const checkBox = document.querySelector("#deposit-check");
+
+const additionalIncomes = document.querySelectorAll(".additional_income-item");
+
+const values = document.getElementsByClassName("result")[0].querySelectorAll("input");
+
+const salaryAmount = document.querySelector('.salary-amount');
+
+const incomeTitle = document.querySelector('.income-title');
+
+const incomeAmount = document.querySelector('.income-amount');
+
+const addIncomeItems = document.querySelectorAll('.additional_income-item');
+
+const expensesTitle = document.querySelector('.expenses-title');
+
+const expensesAmount= document.querySelector('.expenses-amount');
+
+const addExpenses= document.querySelector('.additional_expenses');
+
+const targetAmount= document.querySelector('.target-amount');
+
+
+
+
+
+
+
+
